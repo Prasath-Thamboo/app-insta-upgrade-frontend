ace69f00cd82e33c915d8eb58cd52352a2f36ad8
Prasath.Thamboo <prasath1@hotmail.fr>
2025-07-22 10:41:29 +0200

    username

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,18 +2,18 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './App.css';
 
-
 export default function Login() {
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch('https://ig-counter-backend.onrender.com/login', {
+    const res = await fetch('http://localhost:3001/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ password }),
+      body: JSON.stringify({ username, password }),
     });
 
     const data = await res.json();
@@ -22,21 +22,35 @@ export default function Login() {
       localStorage.setItem('token', data.token);
       navigate('/dashboard');
     } else {
-      alert('Mot de passe incorrect');
+      alert(data.message || 'Identifiants incorrects');
     }
   };
 
   return (
-    
     <form onSubmit={handleSubmit}>
+      <h1>
+          Bienvenue au compteur de followers
+      </h1>
+      <p>
+        Connectez-vous juste en dessous
+      </p>
+      <img src="/insta-logo.png" alt="" />
       <div>
         <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Mot de passe"
-      />
-      <button type="submit">Connexion</button>
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Identifiant"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Mot de passe"
+          required
+        />
+        <button type="submit">Connexion</button>
       </div>
     </form>
   );
